80b9c1f5a420c3c51ba0b5570737f46e320884fe
Devjaksalah <jakanesalah@gmail.com>
2022-02-05 15:32:09 +0100

    tp9

---
+++ src/main/java/dao/FactureDaoImpl.java
@@ -0,0 +1,38 @@
+package dao;
+
+import models.Facture;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+
+public class FactureDaoImpl implements IFactureDao {
+
+    EntityManagerFactory emf = Persistence.createEntityManagerFactory("unit_factures");
+    EntityManager em =emf.createEntityManager();
+
+    public FactureDaoImpl(){
+        System.out.println(("dao created"));
+    }
+
+    @Override
+    public void save(Facture f) {
+        em.getTransaction().begin();
+        em.persist(f);
+        em.getTransaction().commit();
+    }
+
+
+    /*
+    @Override
+    public void save(Facture f) {
+
+        em.getTransaction().begin();
+        em.persist(f);
+        em.getTransaction().commit();
+
+
+     */
+
+}
